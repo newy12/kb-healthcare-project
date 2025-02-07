4210cec0f10d8dc939f8ffecf598e9e4d7f68435
youngjae <newy12@naver.com>
2025-02-07 19:17:25 +0900

    #feat. first commit

---
+++ src/main/java/kr/co/kbhealthcareproject/KbHealthcareProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kbhealthcareproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbHealthcareProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KbHealthcareProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/kbhealthcareproject/KbHealthcareProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kbhealthcareproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbHealthcareProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
